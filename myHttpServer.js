5a430cc08dbf97228017d9bcf54c5306d6ce5e89
ybiran <ybiran@live.com>
2014-10-13 15:17:59 -0700

    Update myHttpServer.js

--- myHttpServer.js
+++ myHttpServer.js
@@ -2,11 +2,11 @@ var http = require('http');
 var os = require('os');
 var dns = require('dns');
 var port = process.env.VCAP_APP_PORT;
-var cloud = process.env.CLOUD_PROVIDER;
+var ver = 'Ver 1.0';
 //var port = 1239;
 var addr;
 dns.lookup(os.hostname(),4,function (err, addr, fam) {
-var platform = os.hostname()+' '+os.type()+' '+os.arch()+' '+addr+' '+cloud;
+var platform = os.hostname()+' '+os.type()+' '+os.arch()+' '+addr+' '+ver;
   http.createServer(function (req, res) {
        res.writeHead(200, {'Content-Type': 'text/plain'});
        res.end('Im running in '+platform+'\n');
