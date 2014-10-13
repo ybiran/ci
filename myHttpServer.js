3c1e358b4ac6ed2ef631a669f7602cba17d8ca13
ybiran <ybiran@live.com>
2014-10-12 20:48:37 -0700

    trying manual port 1239

--- myHttpServer.js
+++ myHttpServer.js
@@ -1,7 +1,8 @@
 var http = require('http');
 var os = require('os');
 var dns = require('dns');
-var port = process.env.VCAP_APP_PORT;
+//var port = process.env.VCAP_APP_PORT;
+var port = 1239;
 var addr;
 dns.lookup(os.hostname(),4,function (err, addr, fam) {
 var platform = os.hostname()+' '+os.type()+' '+os.arch()+' '+addr;
