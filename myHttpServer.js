184bb2e1a76386def0d14fe424194815a6d0e5a6
ybiran <ybiran@live.com>
2014-10-13 17:47:58 -0700

    Update myHttpServer.js

--- myHttpServer.js
+++ myHttpServer.js
@@ -2,7 +2,7 @@ var http = require('http');
 var os = require('os');
 var dns = require('dns');
 var port = process.env.VCAP_APP_PORT;
-var ver = 'Ver 1.1';
+var ver = 'Ver A/B 1.5';
 //var port = 1239;
 var addr;
 dns.lookup(os.hostname(),4,function (err, addr, fam) {
