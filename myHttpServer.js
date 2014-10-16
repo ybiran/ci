37783c737fbc22e6df43fc386dec91633e5080be
Yahav Biran <ybiran@live.com>
2014-10-16 08:52:06 -0700

    add date support

--- myHttpServer.js
+++ myHttpServer.js
@@ -1,13 +1,13 @@
 var http = require('http');
 var os = require('os');
-var date = require('Date');
+var date = require('moment');
 var dns = require('dns');
 var port = process.env.VCAP_APP_PORT;
 var ver = 'Ver A/B 1.6 Reza';
 //var port = 1239;
 var addr;
 dns.lookup(os.hostname(),4,function (err, addr, fam) {
-var platform = os.hostname()+' '+os.type()+' '+os.arch()+' '+addr+' '+ver+' '+date.toISOString();
+var platform = os.hostname()+' '+os.type()+' '+os.arch()+' '+addr+' '+ver+' '+date().format();
   http.createServer(function (req, res) {
        res.writeHead(200, {'Content-Type': 'text/plain'});
        res.end('Im running in '+platform+'\n');
