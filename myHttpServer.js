6c89fd75dd604e8466418b5425925b3b4ee5920e
Yahav Biran <ybiran@live.com>
2014-10-16 17:15:09 -0700

    Update myHttpServer.js

--- myHttpServer.js
+++ myHttpServer.js
@@ -6,7 +6,7 @@ var port = process.env.VCAP_APP_PORT;
 var ver = 'Ver A/B 1.6';
 var addr;
 dns.lookup(os.hostname(),4,function (err, addr, fam) {
-var platform = os.hostname()+' '+os.type()+' '+os.arch()+' '+addr+' '+ver+' '+date().format();
+var platform = os.hostname()+' '+os.type()+' '+os.arch()+' '+addr+' '+ver+' '+ date().format();
   http.createServer(function (req, res) {
        res.writeHead(200, {'Content-Type': 'text/plain'});
        res.end('Im running in '+platform+'\n');
